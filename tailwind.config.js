65844540480dcf87f5ce0bae63d365b243db69c3
Pierina Montalva <pierinamfatur@gmail.com>
2022-10-11 09:48:12 -0500

    se agrego configuracion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+  ],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      'primarycolor': '#6F3DFD',
+      'textcolor': '#202020',
+      'white': '#FFFFFF'
+      
+    },
+    fontFamily: {
+      sans: ['Archivo', 'sans-serif']
+    },
+    fontSize: {
+      sm: '1rem',
+      base: '1.25rem',
+      xl: '2.5rem',
+    }
+  },
+  plugins: [
+  ],
+}
